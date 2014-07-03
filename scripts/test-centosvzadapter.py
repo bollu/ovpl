35de830e9bb120274ff0751f7321039c46b2bbae
Siddharth <siddu.druid@gmail.com>
2014-07-03 02:21:01 +0000

    load correctly

--- scripts/test-centosvzadapter.py
+++ scripts/test-centosvzadapter.py
@@ -6,9 +6,10 @@ import json
 def test(logger):
     """tests the CentOSVZAdapter code by creating and deploying a lab"""
     
-    adapters_conf = json.loads("../src/adapters/config.json")
-    adapters_conf[""] = "CentOSVZAdapter"
-    json.dumps(adapters_conf)
+    
+    adapters_conf = json.load(open("../src/adapters/config.json"))
+    adapters_conf["ADAPTER_NAME"] = "CentOSVZAdapter"
+    json.dump(adapters_conf, open("../src/adapters/config.json"))
 
     import AdapterServer
     import ControllerServer
