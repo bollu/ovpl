85275088dc6bad9e8abcf28457731d8270525c51
Siddharth <siddu.druid@gmail.com>
2014-07-03 12:04:30 +0000

    used multiprocess to spawn off processes

--- scripts/test-centosvzadapter.py
+++ scripts/test-centosvzadapter.py
@@ -1,8 +1,20 @@
-import sys, os, tornado
+import sys, os, plumbum
+from multiprocessing import Process
+
+
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'src'))
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'src', 'adapters'))
 
 import json
+
+def start_controller_server():
+    plumbum.local["python"]["ControllerServer.py"]()
+
+
+def start_adapter_server():
+    plumbum.local["python"]["AdapterServer.py"]()
+
+
 def test(logger):
     """tests the CentOSVZAdapter code by creating and deploying a lab"""
     
@@ -10,10 +22,16 @@ def test(logger):
     adapters_conf["ADAPTER_NAME"] = "CentOSVZAdapter"
     json.dump(adapters_conf, open("../src/adapters/config.json", "w"))
     
+    with plumbum.local.cwd("../src"):
+        p = Process(target=start_controller_server)
+        p.start()
+    
+    with plumbum.local.cwd("../src/adapters"):
+        p = Process(target=start_adapter_server)
+        p.start()
    
-    import ControllerServer
-    import AdapterServer
-
+    #sleep for a second
+    sleep(1)
     logger.info("starting test")
     return True
 
