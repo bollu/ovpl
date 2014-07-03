ac069a81a217f8672cc92dfc7a09224d766b4c85
Siddharth <siddu.druid@gmail.com>
2014-07-03 07:15:50 +0000

    ping travis #3

--- scripts/test-centosvzadapter.py
+++ scripts/test-centosvzadapter.py
@@ -6,7 +6,6 @@ import json
 def test(logger):
     """tests the CentOSVZAdapter code by creating and deploying a lab"""
     
-    
     adapters_conf = json.load(open("../src/adapters/config.json"))
     adapters_conf["ADAPTER_NAME"] = "CentOSVZAdapter"
     json.dump(adapters_conf, open("../src/adapters/config.json", "w"))
